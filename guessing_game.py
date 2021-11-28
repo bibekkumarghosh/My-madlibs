1f7541a6d40b3595574962a68dcc6f8ace259a63
bibekkumarghosh <37596223+bibekkumarghosh@users.noreply.github.com>
2021-11-28 17:40:13 +0530

    added computer_guess()
    added the part where the computer makes the guesses instead of the user.

--- guessing_game.py
+++ guessing_game.py
@@ -1,6 +1,6 @@
 import random
 
-
+#these parts are where the user makes the guess and the computer tells if the guess is correct or not...
 def guess(x):
     random_number = random.randint(1, x)
     guessed = 0
@@ -26,4 +26,27 @@ def guess_color():
     print(f'You have guessed the color {guessed} correctly.')
 
 
-guess_color()
+#guess_color()
+
+
+#this part is where the computer makes the guesses and the user answers whether the guesses made are correct or no.
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    guessed = 0
+    while feedback != 'c':
+        if low != high:
+            guessed = random.randint(low, high)
+        else:
+            guessed = low
+        feedback = input(f'Is {guessed} too high (H), too low (L), or correct (C)??').lower()
+        if feedback == 'h':
+            high = guessed - 1
+        elif feedback == 'l':
+            low = guessed + 1
+
+    print(f'Ysy! The computer guessed your number, {guessed}, correctly!')
+
+
+computer_guess(1000)
